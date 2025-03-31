fddcef025668791f34c555f174043b8ddc975b70
Alexandre Pujol <alexandre@pujol.io>
2025-03-31 22:50:29 +0200

    chore: set license

--- cmd/prebuild/main.go
+++ cmd/prebuild/main.go
@@ -1,6 +1,6 @@
 // play - Apparmor play machine
 // Copyright (C) 2025 Alexandre Pujol <alexandre@pujol.io>
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: GPL-2.0-only
 
 package main
 
